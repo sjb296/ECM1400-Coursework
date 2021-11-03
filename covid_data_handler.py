1e6acdf71623d8b5e3b34ae940a763e623ed56ae
Sam Barker <sambarker247@gmail.com>
2021-11-03 16:50:14 +0000

    Implemented all of process_covid_csv_data and its test except the number of cases in the last 7 days.

--- covid_data_handler.py
+++ covid_data_handler.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 def parse_csv_data(csv_filename: str) -> List[str]:
 	"""
@@ -6,3 +6,27 @@ def parse_csv_data(csv_filename: str) -> List[str]:
 	"""
 	with open(csv_filename, "r") as csv:
 		return [i.strip("\n") for i in csv.readlines()]
+
+def process_covid_csv_data(covid_csv_data: List[str]) -> Tuple[int]:
+	"""
+	Return the total COVID cases in the last 7 days, the current number of
+	hospital cases, and the total deaths, given a list of rows of a csv
+	in government format.
+	"""
+	header = covid_csv_data[0].split(",")
+	metrics = header[4:]
+	body = [i.split(",") for i in covid_csv_data[1:]]
+
+	# Calculate the number of cases in the last 7 days.
+	# TODO: how did he get that value? fix this?
+	print([int(i[6]) for i in body[1:8]])
+	cases_in_last_7_days = sum([int(i[6]) for i in body[1:8]])
+	print(cases_in_last_7_days)
+
+	# Retrieve the current number of hospital cases.
+	current_hospital_cases = int(body[0][5])
+
+	# Calculate the cumulative number of COVID deaths.
+	total_deaths = int([i[4] for i in body if i[4]][0])
+
+	return cases_in_last_7_days, current_hospital_cases, total_deaths
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from covid_data_handler import parse_csv_data
+from covid_data_handler import process_covid_csv_data
 
 def test_parse_csv_data():
 	"""
@@ -7,3 +8,15 @@ def test_parse_csv_data():
 	"""
 	data = parse_csv_data("nation_2021-10-28.csv")
 	assert len(data) == 639
+
+def test_process_covid_csv_data():
+	"""
+	Test that process_covid_csv_data returns the correct values
+	for the example file nation_2021-10-28.csv.
+	"""
+	cases_in_last_7_days, current_hospital_cases, total_deaths = \
+	process_covid_csv_data(parse_csv_data("nation_2021-10-28.csv"))
+
+	assert cases_in_last_7_days == 240_299
+	assert current_hospital_cases == 7_019
+	assert total_deaths == 141_544
