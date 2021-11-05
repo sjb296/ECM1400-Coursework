b5f8ebd9c7402bbd7ef9bfcce8042f393aa46485
Sam Barker <sambarker247@gmail.com>
2021-11-05 14:54:41 +0000

    Implemented covid_API_request in basic form and fixed bugs in process_covid_csv_data.

--- covid_data_handler.py
+++ covid_data_handler.py
@@ -1,4 +1,6 @@
 from typing import List, Dict, Tuple
+from uk_covid19 import Cov19API
+import json
 
 def parse_csv_data(csv_filename: str) -> List[str]:
 	"""
@@ -14,19 +16,53 @@ def process_covid_csv_data(covid_csv_data: List[str]) -> Tuple[int]:
 	in government format.
 	"""
 	header = covid_csv_data[0].split(",")
+	print(header)
 	metrics = header[4:]
-	body = [i.split(",") for i in covid_csv_data[1:]]
+	body = [i.split(",") for i in covid_csv_data[1:-1]]
+	for i in body:
+		print(i)
 
 	# Calculate the number of cases in the last 7 days.
-	# TODO: how did he get that value? fix this?
-	print([int(i[6]) for i in body[1:8]])
-	cases_in_last_7_days = sum([int(i[6]) for i in body[1:8]])
-	print(cases_in_last_7_days)
+	cases_in_last_7_days = sum([int(i[6]) for i in body[2:9]])
 
 	# Retrieve the current number of hospital cases.
-	current_hospital_cases = int(body[0][5])
+	if body[0][5]:
+		# There is data for hospital cases
+		current_hospital_cases = int(body[0][5])
+	else:
+		current_hospital_cases = None
 
 	# Calculate the cumulative number of COVID deaths.
-	total_deaths = int([i[4] for i in body if i[4]][0])
+	try:
+		deaths_total = int([i[4] for i in body if i[4]][0])
+	except IndexError:
+		deaths_total = None
 
-	return cases_in_last_7_days, current_hospital_cases, total_deaths
+	return cases_in_last_7_days, current_hospital_cases, deaths_total
+
+def covid_API_request(location: str = "Exeter", \
+					  location_type: str = "ltla") -> Dict:
+	"""
+	Retrieve relevant COVID-19 information using the API and return
+	it in a dictionary.
+	"""
+	filters = [
+		f"areaType={location_type}",
+		f"areaName={location}"
+	]
+
+	structure = {
+		"areaCode" : "areaCode",
+		"areaName" : "areaName",
+		"areaType" : "areaType",
+		"date" : "date",
+		"cumDeaths28DaysByDeathDate" : "cumDeaths28DaysByDeathDate",
+		#"cumDailyNsoDeathsByDeathDate" : "cumDailyNsoDeathsByDeathDate",
+		"hospitalCases" : "hospitalCases",
+		"newCasesBySpecimenDate" : "newCasesBySpecimenDate"
+	}
+
+	api = Cov19API(filters=filters, structure=structure)
+
+	csv_data = api.get_csv()
+	return csv_data
