2cfbfeaca7fc2546b40d199ff26def3874785ac2
Sam Barker <sambarker247@gmail.com>
2021-11-30 14:46:05 +0000

    Fixed news API to properly use covid_terms on an OR basis and not AND

--- services/covid_news_handling.py
+++ services/covid_news_handling.py
@@ -6,7 +6,7 @@ import datetime
 import json
 
 def news_API_request(covid_terms: str \
-						="Covid COVID-19 coronavirus"):
+						="Covid COVID-19 coronavirus") -> Dict:
 	"""
 	Request today's news articles containing the words
 	in covid_terms, and return the result in a dictionary.
@@ -19,5 +19,35 @@ def news_API_request(covid_terms: str \
 		api_key = f.readlines()[0][:-1]
 		api = NewsApiClient(api_key=api_key)
 
-	news = dict(api.get_top_headlines(q=covid_terms))
+	searches_list = []
+	covid_terms_arr = covid_terms.split(" ")
+
+	for i in covid_terms_arr:
+		searches_list.append(dict(api.get_top_headlines(q=i)))
+
+	all_ok = True
+	status = "ok"
+	for i in searches_list:
+		if i["status"] != "ok":
+			print(f"NEWS API STATUS {i['status']}")
+			status = i["status"]
+
+	total_results = sum([i["totalResults"] for i in searches_list])
+
+	articles_dupes = []
+	for i in searches_list:
+		articles_dupes.extend(i["articles"])
+
+	# Remove duplicates
+	articles = []
+	[articles.append(i) for i in articles_dupes if i not in articles]
+
+	news = {
+		"status": status,
+		"totalResults": total_results,
+		"articles": articles
+	}
+
+	print(news)
+
 	return news
